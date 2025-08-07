8cebe27a60714c38b0cbfd162abdad374208cc47
Vinit jain <jain.vinit14@gmail.com>
2025-08-06 23:13:47 -0400

    added output parser and its working well

--- routes/ai.js
+++ routes/ai.js
@@ -1,6 +1,8 @@
-const Ollama = require("@langchain/ollama");
 var express = require('express');
 var router = express.Router();
+const Ollama = require("@langchain/ollama");
+const { StructuredOutputParser } = require('@langchain/core/output_parsers')
+const { z } = require('zod');
 
 const llm = new Ollama.Ollama({
     model: "gemma3:4b",
@@ -8,12 +10,30 @@ const llm = new Ollama.Ollama({
     maxRetries: 2,
 });
 
+const parser = StructuredOutputParser.fromZodSchema(
+    z.object({
+        name: z.string().nullable(),
+        email: z.string().email().nullable(),
+        experience: z.array(z.string().nullable()).nullable(),
+        education: z.array(z.string().nullable()).nullable()
+    })
+);
+
+const formatInstructions = parser.getFormatInstructions();
+
+
 router.post('/v1/parseresume', async function (req, res, next) {
     const rawPDF = req.body.rawpdf;
 
-    const completion = await llm.invoke(rawPDF);
-    console.log(completion);
-    res.send(completion);
+    const prompt = `
+    Extract the following information from the resume text.
+    ${formatInstructions}
+
+    Raw PDF Resume extract:
+    ${rawPDF}`;
+
+    const result = await parser.parse(await llm.invoke(prompt));
+    res.send(result);
 });
 
 module.exports = router;
